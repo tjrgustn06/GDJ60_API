fe62da501d2af507ed90dc33a57f9ace86b9f8f6
tjrgustn06 <tjrgustn06@naver.com>
2023-01-06 10:46:41 +0900

    e

--- src/com/iu/api1/Strings/StringEx2.java
+++ src/com/iu/api1/Strings/StringEx2.java
@@ -6,26 +6,35 @@ public class StringEx2 {
 
 	public static void main(String[] args) {		
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Start");
 		String [] names = {"jpg", "jpeg", "gif", "png"};
 		System.out.println("파일명을 입력");
-		String fileName = sc.next();
+		String fileName = sc.next();//abc.jpg
+		boolean flag = true;
 		
-		for(int i =0; i<names.length; i++) {
-			System.out.println(i);
-			if(iu == fileName.substring(i)) {
-				System.out.println(i);
+		//확장자 분리 작업
+		int idx = fileName.lastIndexOf(".");
+		String result = fileName.substring(idx+1);
+		System.out.println(result);
+		
+		
+		//확장자가 이미지인지 검증
+		for(int i =0; i<names.length;i++) {
+			if(result.equals(names[i])) {
+				System.out.println("정상 파일입니다");
+				i = names.length;
+				//flag =flase; (밑에꺼랑 같은 의미임)
+				flag=!flag;
+				break;
 			}
 			
 		}
+	System.out.println("비정상");
+
 		
-		
-		
-		
-		
-		
-		
-		
+		System.out.println("Finish");
 
 	}
-
+	
+}
 
